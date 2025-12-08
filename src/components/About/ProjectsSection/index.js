bbb62030b325a1fc8c4773b94a129bcb6cacc6da
maksluczak <maksluczak99@gmail.com>
2025-12-08 20:50:11 +0100

    added necessery links

--- src/components/About/ProjectsSection/index.js
+++ src/components/About/ProjectsSection/index.js
@@ -18,17 +18,17 @@ const ProjectSection = ({activeSection, setActiveSection}) => {
     {
       name: 'AllerNote',
       description: 'A three-tier application for allergy management: <b>Next.js</b> frontend for tracking pollen intensity and symptoms, <b>Express</b> backend handling API logic and authentication, <b>MongoDB</b> database storing user data, notes and history.',
-      link: '#',
+      link: 'https://github.com/maksluczak/aller-note-2.0',
     },
     {
       name: 'Snake in Java',
       description: 'A simple window-based <b>Java</b> Snake game built with <b>Swing</b>, featuring keyboard controls, dynamic movement, collision detection, and a scoring system.',
-      link: '#',
+      link: 'https://github.com/maksluczak/snake-java',
     },
     {
       name: 'E-Commerce App',
       description: 'A three-tier e-commerce application built with <b>Next.js</b> and TypeScript, <b>Express</b>, and <b>MongoDB</b> for data storage. Integrated with Stripe API to simulate secure payments.',
-      link: '#',
+      link: 'https://github.com/KISiM-AGH/projekt-zaliczeniowy-ii-2025-kacyki',
     },
     {
       name: 'File Upload App',
