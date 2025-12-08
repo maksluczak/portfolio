7f52b75d8e71d8e5ba960fcea6efd9518799ce78
maksluczak <maksluczak99@gmail.com>
2025-12-08 20:39:18 +0100

    contact page

--- src/components/Contact/ContactRotation/index.js
+++ src/components/Contact/ContactRotation/index.js
@@ -4,14 +4,30 @@ import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-i
 
 const ContactRotation = () => {
   const contacts = [
-    { icon: faFacebook, link: 'https://www.facebook.com' },
-    { icon: faLinkedin, link: 'https://www.linkedin.com' },
-    { icon: faGithub, link: 'https://github.com' },
-  ]
+    { icon: faFacebook, link: 'https://www.facebook.com/profile.php?id=100015286782095' },
+    { icon: faLinkedin, link: 'https://www.linkedin.com/in/maksymilian-łuczak-69a10b331' },
+    { icon: faGithub, link: 'https://github.com/maksluczak?tab=repositories' },
+  ];
+
+  const animationDuration = 12;
+  const totalItems = contacts.length;
+  const delayIncrement = animationDuration / totalItems;
 
   return (
-    <div>
-      
+    <div className="contact-rotation-container">
+
+      {contacts.map((item, index) => (
+        <a
+          key={index}
+          href={item.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rotation-item"
+          style={{ animationDelay: `-${index * delayIncrement}s` }}
+        >
+          <FontAwesomeIcon icon={item.icon} color="#08CB00" />
+        </a>
+      ))}
     </div>
   );
 }
